a5338252185569976fd0bec890841057c9ab5e20
AlmiroN <33394132+AlmiroN-code@users.noreply.github.com>
2025-08-19 14:28:09 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+api_id = 28033048
+api_hash = 'cc3a08792b1a0a93b9d4883d83247233'
+bot_token = '7195868064:AAEUNnghpKfWK6t51cE8p3FC8A_VK3N9x7s'
+my_id = 5360044722
+
+technical_channel_id = -1002448240004  # ID технического канала
+
+new_link = "http://t.me/"
+new_username = "@droid_mods"
+
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
